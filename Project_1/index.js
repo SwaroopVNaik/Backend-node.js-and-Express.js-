6d2d093a8028fad8664175190eef86768fa4f497
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-06-03 11:31:03 +0530

    Dyanmic Routing, Setting up parsers for form, setting up ESJ for ESJ, Setting up Publc static files

--- Project_1/index.js
+++ Project_1/index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// send() will directly send the data to client.
+// render() will renders a templete/view in HTML to client.
 
 // Parsers
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+
+// Render Pages 
 app.set('view engine', 'ejs');
-app.use(express.static(Path2D.join(__dirname, 'public')))
+
+// using this to find all the static files in the Public folder for all the Request 
+// __dirname => Path of Current Directory 
+// Public => the Static file folder name 
+// both path are Joined using join 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(request, response){
-    response.render("index")
+    response.render("index");
+})
+
+// To make the The url dynamic -> find out the route in url which can be dynamic and then just add a colon : 
+// "/profile/swaroop"  =>>> "/profile/:swaroop"
+
+app.get("/profile/:username", function(request, response){
+    response.send(request.params.username);
 })
 
 app.listen(3000, function(){
     console.log("Its Running");
-})555555
+})
